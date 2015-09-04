58b065ac804b145948c98fe030d5b40f13a6be7c
Melanie Freeman <mdf@andrew.cmu.edu>
2015-09-04 16:23:53 -0400

    Update to git ignore, changed dependencies in app.js

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-var express = require("express");
-var morgan = require("morgan");
-// to be used later once mongodb functionality is added
-//var dbRoutes = require('./routes/dbRoutes'); 
+var express = require('express'),
+    bson = require('bson'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'), //used to manipulate POST, DELETE, etc
+    
+    attractions = require('./routes/attractions'),
+    users = require('./routes/users'),
+    home = require('./routes/home'),
+    
+    db = require('./models/db');
+    
 var app = express();
 
-var bodyParser = require('body-parser')
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({    // to support URL-encoded bodies
   extended: true
-})); 
+}));
+app.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+}));
 
 app.set('views', __dirname + '/views');
 
 app.use(morgan('tiny'));	// Log requests
 
-// Define the view (templating) engine. Similar to erb.
-app.set('view engine', 'ejs');
+// Define the view (templating) engine.
+app.set('view engine', 'jade');
 
 // Define that static content such as html is in public directory
 app.use(express.static(__dirname + '/public'));
@@ -23,17 +41,44 @@ app.use(express.static(__dirname + '/public'));
 
 /* 
 	## Example of how to build crud operations with express app ##
-	   The get operations are defined in dbRoutes.js which links to the model.
+	   The operations are defined in dbRoutes.js which links to the model. */
 
-app.get('/:collection/:firstname?/:lastname?', dbRoutes.getArtist);
-app.post('/:collection/:firstname/:lastname', dbRoutes.postArtist);
-app.put('/:collection/:firstname/:lastname/:band/:genre', dbRoutes.putArtist);
-app.delete('/:collection/:firstname/:lastname', dbRoutes.deleteArtist);
-
-*/
+// ROUTES
+app.use('/attractions', attractions)
+app.use('/users', users)
+app.use('/', home)
 
 // specified port for running app
 app.listen(50000);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-console.log("Server listening at http://localhost:50000/");
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
