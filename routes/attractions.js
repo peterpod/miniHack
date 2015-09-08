836ca96646c54a30ea785a236a09a83e8f0027dd
Melanie Freeman <mdf@andrew.cmu.edu>
2015-09-08 12:20:46 -0400

    cleaned up edit attraction page

--- routes/attractions.js
+++ routes/attractions.js
@@ -1,16 +1,15 @@
 var mongoose = require("mongoose"),
-    express = require('express'),
-    router = express.Router(),
-    bodyParser = require("body-parser"),
-    
-    //File upload requirements
-    multer  = require('multer'),
-    upload = multer({ dest: 'public/uploads/' }),
-    http = require('http'),
-    url = require('url'),
-    
-    Attraction = require('../models/attraction');
-
+  express = require('express'),
+  router = express.Router(),
+  bodyParser = require("body-parser"),
+  
+  //File upload requirements
+  multer  = require('multer'),
+  upload = multer({ dest: 'public/uploads/' }),
+  http = require('http'),
+  url = require('url'),
+  
+  Attraction = require('../models/attraction');
 
 // Set the pageName; used for setting navbar links active
 router.use(function(req, res, next){
@@ -29,7 +28,7 @@ router.get('/', function(req, res) {
     else if (req.query.type == "Food") { subPageName = "Dining" }
     else { subPageName = "Events" };
   };
-	Attraction.find(conditions, null, {sort: {"created_at":-1}}, function (err,attractions) {
+  Attraction.find(conditions, null, {sort: {"created_at":-1}}, function (err,attractions) {
     if (err) {
       return console.error(err);
     } else {
@@ -38,12 +37,12 @@ router.get('/', function(req, res) {
   });
 });
 
-
 // New attaction page
 router.get('/new', function(req, res) {
     if (!req.user){ res.redirect('/'); }
     else {
-      res.render('attractions/new');
+      res.render('attractions/new', 
+                  { "attraction" : {}} );
     }
 });
 
@@ -72,75 +71,6 @@ router.use(function(req, res, next) {
     }
 });
 
-// Create an attraction
-router.post('/', upload.single('photo'), function(req, res) {
-  var category = req.body.category;
-  var business = new Array ("Clothing","Electronics");
-  var food = new Array ("Truck, Restaurant");
-  var type; var photo;
-  if (business.indexOf(category) > -1) {type = "Business";}
-  else if (food.indexOf(category) > -1) {type = "Food"; }
-  else { type = "Event"; }
-  // Slice the /public off of the file path.
-  // Use absolute url of the photo so that the url can be used easily in different levels,
-  // i.e attractions (1 level) and users/:id (2 levels).
-  if (req.file) { photo = 'http://' + req.headers.host + '/' + req.file.path.slice(7) };
-  
-  Attraction.create({
-      user_id:req.user._id,
-      type:type,
-      category:req.body.category,
-      title:req.body.title,
-      description:req.body.description,
-      address:req.body.address,
-      city:req.body.city,
-      state:req.body.state,
-      zip:req.body.zip,
-      dollar:req.body.dollar,
-      photo: photo
-    }, function(err,attraction) {
-      if (err) { res.send('POST attraction/ error: ' + err)}
-      else {
-        req.flash('success', attraction.title.type + ' "' + attraction.title.description + '" successfully posted.');
-        res.redirect("/attractions");
-        }
-      });
-  });
-
-//find specific attraction
-router.route('/:id')
-    //Update
-    .put(function(req, res) {
-      Attraction.findById(req.params.id, function(err,attraction) {
-        attraction.update({
-          title:req.body.title,
-          description:req.body.description,
-          updated_at: Date.now
-        }, function (err, attractionID) {
-          if (err) {
-            res.send('PUT attraction/:id error: ' + err)
-          } else {
-            res.redirect("/attractions/" + attraction._id)
-          }
-        });
-      });
-    })
-    //DELETE
-    .delete(function(req,res) {
-      Attraction.findById(req.params.id, function(err,attraction) {
-        if (err) { return console.error(err); }
-        else {
-              attraction.remove(function (err, attraction){
-                  if (err) {return console.error(err);}
-                  else {
-                    req.flash('success', attraction.title.type + ' "' + attraction.title.description + '" successfully deleted.');
-                    res.redirect("/attractions");
-                    }
-                });
-              }
-      });
-    });
-    
 // Edit attraction page
 router.get('/:id/edit', function(req, res) {
     Attraction.findById(req.params.id, function (err, attraction) {
@@ -153,6 +83,87 @@ router.get('/:id/edit', function(req, res) {
                  }
             });
         });
+
+//DELETE attraction
+router.delete('/:id', function(req,res) {
+  Attraction.findById(req.params.id, function(er,attraction) {
+    if (er) { return console.error(er); }
+    else {
+          attraction.remove(function (err, attraction){
+              if (err) {return console.error(err);}
+              else {
+                req.flash('success', '"' + 'attraction.title' + '" successfully deleted.');
+                res.redirect("/attractions");
+                }
+            });
+          }
+  });
+});
+
+// Create an attraction
+router.post('/', upload.single('photo'), function(req, res) {
+  // Slice the /public off of the file path.
+  // Use absolute url of the photo so that the url can be used easily in different levels,
+  // i.e attractions (1 level) and users/:id (2 levels).
+  var photo;
+  if (req.file) {
+    photo = 'http://' + req.headers.host + '/' + req.file.path.slice(7)
+  } else {
+    photo = 'https://placeholdit.imgix.net/~text?txtsize=33&txt=No+Image&w=200&h=200'; //placeholder image
+  };
+  Attraction.create({
+      user_id:req.user._id,
+      category:req.body.category,
+      title:req.body.title,
+      description:req.body.description,
+      address:req.body.address,
+      city:req.body.city,
+      state:req.body.state,
+      zip:req.body.zip,
+      dollar:req.body.dollar,
+      photo: photo
+    }, function(err,attraction) {
+      if (err) { res.send('POST attraction/ error: ' + err)}
+      else {
+        req.flash('success', '"'+attraction.title+'" successfully posted.');
+        res.redirect("/attractions");
+        }
+    }
+  );
+});
+
+//Update attraction
+router.post('/:id', upload.single('photo'), function(req, res) {
+  var photo;
+  if (req.file) { photo = 'http://' + req.headers.host + '/' + req.file.path.slice(7) };
+  Attraction.findById(req.params.id, function(err,attraction) {
+    if (err) { return console.error(err); }
+    else { attraction.update({
+      category:req.body.category,
+      title:req.body.title,
+      description:req.body.description,
+      address:req.body.address,
+      city:req.body.city,
+      state:req.body.state,
+      zip:req.body.zip,
+      dollar:req.body.dollar,
+      photo: photo
+    }, function (errTwo, update) {
+      if (errTwo) {
+        res.send('PUT attraction/:id error: ' + errTwo)
+      } else {
+        Attraction.findById(req.params.id, function(errThree,attractionTwo) {
+          if (errThree) { return console.error(errThree); }
+          else {
+            req.flash('success', '"' + attractionTwo.title + '" successfully updated.');
+            res.redirect("/attractions/" + req.params.id);
+          };
+        });
+      }
+    });
+    }
+  });
+})
 
 module.exports = router;
 
